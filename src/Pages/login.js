ec81215b978a2d2ac56c82961ded814e6f719609
Davi Serrano <daviscardoso2901@gmail.com>
2021-07-05 00:40:23 -0300

    login desing terminado

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,9 +1,32 @@
-import react from "react" 
+import "./login.css" 
+
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
 
 function Login () {
 
     return (
-        <h1> Login</h1>
+        
+        <div className="Login">
+            
+            <Header />
+            <div className="boxcenter">
+                <div className="center">
+
+                    <h2>Login</h2>
+                    
+                    <div className="log">
+                        <label>Email</label>
+                        <input type="email" />
+                        <label>Password</label>
+                        <input type="password"/>
+                        <div className="button">login</div>  
+                    </div>   
+                </div>
+            </div>
+      
+            <Footer />
+       </div>
     )
 }
 
